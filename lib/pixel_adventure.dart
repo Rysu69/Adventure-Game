e74aaed3d2da944534c2c250b5b20ba84ea5e17f
Rysu69 <141984337+Rysu69@users.noreply.github.com>
2023-10-31 08:39:23 +0800

    fix lvl bug

--- lib/pixel_adventure.dart
+++ lib/pixel_adventure.dart
@@ -25,7 +25,7 @@ class PixelAdventure extends FlameGame
   bool showControls = false;
 
   bool playSounds = true;
-  double soundVolume = 1.0;
+  double soundVolume = 0.3;
     List<String> levelNames = [ 'Level-01', 'Level-02', 'Level-03', 'Level-04', 'Level-05' ];
   int currentLevelIndex = 0;
 
